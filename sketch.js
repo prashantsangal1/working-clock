ebbf298df58f78a1fc5df745dee0a9e12fc40248
prashantsangal1 <69078671+prashantsangal1@users.noreply.github.com>
2020-12-05 14:58:54 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,13 +3,6 @@ var hrAngle, mnAngle, scAngle;
 
 function setup() {
   createCanvas(800,800);
-  hr = hour();
-  mn = minute();
-  sc = second();
-
-  scAngle = map(sc,0,60,0,360)
-  hrAngle = map(hr,0,24,0,360)
-  mnAngle = map(mn,0,60,0,360)
 }
 
 function draw() {
@@ -17,17 +10,27 @@ function draw() {
 
   angleMode(DEGREES);
 
+  hr = hour();
+  mn = minute();
+  sc = second();
+
+  scAngle = map(sc,0,60,0,360)
+  hrAngle = map(hr,0,24,0,360)
+  mnAngle = map(mn,0,60,0,360)
+
+  translate(200,200);
   push();
     rotate(scAngle);
     stroke(255,0,0);
     strokeWeight(7);
     line(0,0,100,0);
-    translate(400,400);
+    noFill();
+    arc(0,0,200,200,scAngle,2)
   pop();
 
   push();
     rotate(mnAngle);
-    stroke(255,0,0);
+    stroke(0,0,255);
     strokeWeight(10);
     line(0,0,100,0);
     translate(400,400)
@@ -38,10 +41,8 @@ function draw() {
     stroke(0,255,0);
     strokeWeight(7);
     line(0,0,100,0);
-    translate(400,400)
+    translate(400,400);
   pop();
 
   drawSprites();
 }
-
-//please help it doesnt work i have no idea what to do here im tired
